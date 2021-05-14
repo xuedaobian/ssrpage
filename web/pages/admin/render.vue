c9b641270e827634f4964d7fbebb00c649e09d1d
John Smith <1689359591@qq.com>
2021-05-14 11:07:16 +0800

    完善上传图片，将md文件转移到腾讯云存储

--- web/pages/admin/render.vue
+++ web/pages/admin/render.vue
@@ -20,41 +20,55 @@ export default {
   data() {
     return {
       text: "",
+      loginState: false,
+      cloudApp: {},
     };
   },
   created(){
-
+    // this.login();
   },
   methods: {
-    async login(files){
+    async login(){
       console.log('login')
       const app = cloudbase.init({
         env: 'note-16440d'
       });
-
       const auth = app.auth();
       await auth.anonymousAuthProvider().signIn();
       // 匿名登录成功检测登录状态isAnonymous字段为true
       const loginState = await auth.getLoginState();
+
+      this.cloudApp = app;
+      this.loginState = loginState.isAnonymousAuth;
       console.log(loginState.isAnonymousAuth); // true
-      return await app
+      
+    },
+
+    saveFile(text,html) {
+      console.log(text);
+      console.log(html)
+
+    },
+    async uploadImage(e, insertImage,files) {
+      if(!this.loginState) await this.login()
+      // const fileUrl = await this.upCloudImage(files)
+      this.cloudApp
       .uploadFile({
         // 云存储的路径
         cloudPath: `images/${files[0].name}`,
         // 需要上传的文件，File 类型
         filePath: files[0]
-      }).fileID
-    },
-    saveFile(text) {
-      console.log(text);
+      }).then(res => {
+        // console.log(res)
+        // return res.fileID
+        this.cloudApp.getTempFileURL({fileList:[res.fileID]}).then(res => {
+          const fileURL = res.fileList[0].tempFileURL;
+            insertImage({
+              url: fileURL,
+              desc:'this is a image'
+          })
 
-    },
-    async uploadImage(e, insertImage,files) {
-      const fileUri = await this.login(files)
-      console.log(fileUrl)
-      insertImage({
-        url: fileUri,
-        desc:''
+        })
       })
     },
   },
--- web/pages/article/fetch.ts
+++ web/pages/article/fetch.ts
@@ -7,14 +7,13 @@ export default async ({ store, router }, ctx?: ISSRContext) => {
   if (__isBrowser__) {
     console.log(router.path)
     path = router.path.replace(/\$/g, '/').replace('/article/', '')
-    data = await axios.get(`/mkdown/${path}.md`)
+    data = await axios.get(`https://6e6f-note-16440d-1258774696.tcb.qcloud.la/mkdown/${path}.md`)
   } else {
     path = ctx?.request.url.replace(/\$/g, '/').replace('/article/', '')
-
     const tempHost = ctx?.request.header.host
     const ho = tempHost.substring(0, tempHost.indexOf(':'))
     const po = tempHost.substring(tempHost.indexOf(':')+1)
-    data = await axios.get(`/mkdown/${path}.md`, {
+    data = await axios.get(`https://6e6f-note-16440d-1258774696.tcb.qcloud.la/mkdown/${path}.md`, {
       proxy: { host: ho, port: po }
     })
   }
